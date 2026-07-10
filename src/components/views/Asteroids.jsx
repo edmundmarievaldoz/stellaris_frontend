19efce43aab1d827467e6dc420e209a64167097d
edmundmarievaldoz <edmundmarievaldoz@gmail.com>
2026-07-10 19:53:47 +0100

    will fix tomorrow

--- src/components/views/Asteroids.jsx
+++ src/components/views/Asteroids.jsx
@@ -8,21 +8,21 @@ function Asteroids () {
     // State --------------------------------
 
     const [asteroid, loadingAsteroidMessage, loadAsteroid] = useLoad(asteroidEndpoint);
+
     // Handlers -----------------------------
     // Views --------------------------------
     return (
         <>
         <h1>Asteroid Tracker</h1>
+        <img src='https://imgs.search.brave.com/i21hDJp6XKoXCVXqUIpAGU9xtIYVhXxwqfLZg4NCWBI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGFy/d2Fsay5zcGFjZS9n/YWxsZXJ5L2ltYWdl/cy9hc3Rlcm9pZC1i/ZWx0LXBvbGwvMTky/MHg1NDAuanBn' alt='asteroid belt' className='asteroidImg' />
 
         <div className='asteroid-info-container'>
-            <img src='https://imgs.search.brave.com/i21hDJp6XKoXCVXqUIpAGU9xtIYVhXxwqfLZg4NCWBI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGFy/d2Fsay5zcGFjZS9n/YWxsZXJ5L2ltYWdl/cy9hc3Rlcm9pZC1i/ZWx0LXBvbGwvMTky/MHg1NDAuanBn' alt='asteroid belt' className='asteroidImg' />
 
             {!asteroid ? (
                 <p>{loadingAsteroidMessage}</p>
             ) :(
                 <>
             <div className='hazard'>
-                
             </div>
 
             <div className='nearEarth'>
